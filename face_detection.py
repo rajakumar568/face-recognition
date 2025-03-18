a32fe33cd1fbeadd44ddaae7563b148bb3220a7f
RAJA KUMAR <rajakumarsah568@gmail.com>
2025-03-18 18:19:33 +0530

    final

---
+++ face_detection.py
@@ -0,0 +1,34 @@
+import cv2
+
+def face_detection():
+   
+    detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+    
+    cap = cv2.VideoCapture(0)
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, "Face", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.7, (0, 255, 0), 2)
+        
+        cv2.imshow("Webcam Face Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    face_detection()
